29358bfd974344bc5609e52e336209dd453e8626
Sandeep Sharma <sandeepisthename007@gmail.com>
2020-07-06 19:50:16 +0530

    commited

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXUGerP3hRTD0k58uFfONbXWe9G8zK3qU",
+  authDomain: "chall-app-94f0f.firebaseapp.com",
+  databaseURL: "https://chall-app-94f0f.firebaseio.com",
+  projectId: "chall-app-94f0f",
+  storageBucket: "chall-app-94f0f.appspot.com",
+  messagingSenderId: "502563467313",
+  appId: "1:502563467313:web:da1ba1e661d8166b17a510",
+  measurementId: "G-9XXLKX56J6"
+}
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
